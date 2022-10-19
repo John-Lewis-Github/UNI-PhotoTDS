d3a7bca1d8e5a3113c0f2c4cb4cd013b6e06158c
Rubén <rubencoll386@gmail.com>
2022-10-19 17:08:32 +0200

    Initial commit

---
+++ PhotoTDS/src/main/java/Uni/JLGG_RCS/PhotoTDS/App.java
@@ -0,0 +1,13 @@
+package Uni.JLGG_RCS.PhotoTDS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
